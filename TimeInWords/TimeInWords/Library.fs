330a1a09f0759fa2fa7f8b0e616a6338bf7ff3a3
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-09-06 13:47:21 +0200

    pass 5 o' clock

---
+++ TimeInWords/TimeInWords/Library.fs
@@ -0,0 +1,21 @@
+﻿namespace TimeInWords
+
+open Xunit
+
+(*
+Given the time in numerals we may convert it into words, as shown below:
+
+At minutes = 00, use o' clock.
+For 1<= minutes <=30, use past, and for minutes > 30 use to.
+Note the space between the apostrophe and clock in o' clock.
+*)
+
+module TimeInWords =
+    let timeToWords hour minute =
+        hour.ToString() + " o' clock"
+
+module Tests =
+    [<Fact>]
+    let ``5 o' clock`` () =
+        let result = TimeInWords.timeToWords 5 00
+        Assert.Equal("5 o' clock", result)
